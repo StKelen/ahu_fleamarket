cd7348b3ba63de0c8b600c0448b41bd76bfe490c
StKelen <st_kalen@qq.com>
2021-01-03 20:57:52 +0800

    init flutter

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('安小转客户端项目初始化'),
+    );
+  }
+}
+
